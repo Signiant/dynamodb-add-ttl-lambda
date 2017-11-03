c3c3d802f61ea0ac574cedab2133161f25741eaf
Dave North <dnorth@signiant.com>
2017-11-03 12:00:24 -0400

    Style

--- lambda/dynamodb-add-ttl.py
+++ lambda/dynamodb-add-ttl.py
@@ -1,4 +1,3 @@
-import pprint
 import os
 import json
 import time
@@ -48,22 +47,60 @@ def get_base_value_epoch_seconds(base_value):
 
 
 def get_expiry(base_value, ttl_duration):
-    print "get_expiry " + str(base_value) + " dur: " + str(ttl_duration)
+    expiry_ttl = None
+
     future = datetime.datetime.fromtimestamp(float(base_value)) + datetime.timedelta(days=long(ttl_duration))
     expiry_ttl = calendar.timegm(future.timetuple())
 
     return expiry_ttl
 
-def update_item(item, client, table_name):
-    print ("updating item " + item + " in table " + table_name)
+
+def update_item(table_name, key, ttl_attrib_name, ttl_attrib_value, client):
+    status = False
+    key_str = json.dumps(key)
+
+    print ("updating item " + key_str + " in table " + table_name)
+
+    response = client.update_item(
+        TableName=table_name,
+        Key=key,
+        ExpressionAttributeNames={
+            '#ED': ttl_attrib_name
+        },
+        ExpressionAttributeValues={
+            ':ed': {
+                'N': str(ttl_attrib_value),
+            }
+        },
+        ReturnValues='ALL_NEW',
+        UpdateExpression='SET #ED = :ed'
+    )
+
+    if 'ResponseMetadata' in response:
+        if 'HTTPStatusCode' in response['ResponseMetadata']:
+            if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+                print "ERROR: error updating key " + key_str
+                status = False
+            else:
+                print "+ Successfully updated " + key_str
+                status = True
+        else:
+            print "ERROR: No http status code in response when trying to update " + key_str
+            status = False
+    else:
+        print "ERROR: No response metadata when trying to update " + key_str
+        status = False
+
+    return status
 
 
 def lambda_handler(event, context):
     status = True
     do_update = False
+    table_name = None
     master_attribute_value = None
 
-    #print("Received event: " + json.dumps(event, indent=2))
+    # print("Received event: " + json.dumps(event, indent=2))
 
     if 'master_attribute' in os.environ:
         master_attribute = os.environ['master_attribute']
@@ -89,14 +126,16 @@ def lambda_handler(event, context):
         record = event['Records'][0]
 
         if record['eventName'] == "INSERT":
-            print "New INSERT into table detected - adding TTL if not already present"
+            table_name = record["eventSourceARN"].split("/")[1]
+
+            print "New INSERT into table " + table_name + " detected - adding TTL if not already present"
 
             # Do we already have a TTL (very unlikely)?
-            if not ttl_attribute_name in record["dynamodb"]["NewImage"]:
+            if ttl_attribute_name not in record["dynamodb"]["NewImage"]:
                 print("no TTL attribute name " + ttl_attribute_name + " found - computing and adding")
 
                 # Does our master TTL attribute exist?
-                if not master_attribute in record["dynamodb"]["NewImage"]:
+                if master_attribute not in record["dynamodb"]["NewImage"]:
                     print("ERROR: The master attribute " + master_attribute + " to base the TTL on does not exist")
                 else:
                     print("Computing a new TTL based on the value in " + master_attribute + " that is " + str(time_to_live_days) + " days in the future")
@@ -117,6 +156,8 @@ def lambda_handler(event, context):
                         if master_epoch_seconds:
                             ttl_value = get_expiry(master_epoch_seconds, time_to_live_days)
                             print("The TTL value is " + str(ttl_value))
+
+                            table_key = record['dynamodb']['Keys']
                             do_update = True
                         else:
                             print("ERROR: Unable to obtain the original timestamp attribute value to compute a TTL")
@@ -128,5 +169,9 @@ def lambda_handler(event, context):
         session = boto3.session.Session(region_name=record['awsRegion'])
         dynamodb_client = session.client('dynamodb')
 
+        if update_item(table_name, table_key, ttl_attribute_name, ttl_value, dynamodb_client):
+            status = True
+        else:
+            status = False
 
     return status
